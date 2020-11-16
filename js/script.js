ad79b70d6a31e081b748fd51feea2704395bdc31
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-16 15:43:24 +0100

    programma base completo

--- js/script.js
+++ js/script.js
@@ -5,21 +5,25 @@
 // della lista in cui il nuovo utente si trova.
 
 // creazione di un array contenente dei cognomi
-var nameList = ["Simpson", "Van Houten", "Burns", "Nahasapeemapetilon"];
-console.log("La lista iniziale è composta da:", nameList);
+var nameList = ["Simpson", "Van Houten", "Burns", "Nahasapeemapetilon", "Skinner", "Lovejoy", "Flanders", "Szyslak", "Prince", "Smithers", "Gumble", "Carlson", "Leonard", "Brockman"];
 
 // chiedere all'utente il proprio cognome
 var customerName = prompt("Inserire il proprio cognome");
-console.log("Il tuo cognome è:", customerName);
 
 // inserirlo all'interno dell'array precedentemente creata
 nameList.push(customerName);
-console.log("La nuova lista è composta da:", nameList);
 
 // stampare la lista dei cognomi in ordine alfabetico
+// determinare posizione umana utente
 nameList.sort();
-for (var i = 0; i < nameList.length; i++) {
-     console.log(nameList[i]);
-}
 
-// console.log("Il tuo cognome si trova in posizione:", nameList.length);
+var i = 0;
+while (i < nameList.length) {
+     document.getElementById("name_list").innerHTML += "<li>" + nameList[i] + "</li>";
+
+     if (nameList[i] == customerName) {
+          document.getElementById("position").innerHTML = i + 1;
+     }
+     
+     i++;
+}
